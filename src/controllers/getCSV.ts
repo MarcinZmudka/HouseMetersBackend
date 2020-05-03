c699d7bbc347b53b74d635f303da310db19ff5a6
unknown <marcinzmudka@gmail.com>
2020-05-03 22:55:51 +0200

    add new schema to getCSV

--- src/controllers/getCSV.ts
+++ src/controllers/getCSV.ts
@@ -1,61 +1,22 @@
 import { Request, Response } from "express";
 import { data } from "../models/Data";
 import { parse } from "json2csv";
-interface flat {
+interface Flat {
 	id: number;
 	hotWater: number;
 	coldWater: number;
 	electricityOne: number;
-	electricityTwo: number;
 	heat: number;
 }
-interface mongoData {
+export interface House extends Flat {
+	electricityTwo: number;
+	togetherOne: number;
+	togetherTwo: number;
+}
+interface MongoData {
 	createdAt: Date;
-	flats: flat[];
+	flats: Array<Flat | House>;
 }
-export default function getCSV(req: Request, res: Response) {
-	data.findOne({}, {}, { sort: { createdAt: -1 } }, function (
-		err: Error,
-		post
-	) {
-		if (err) {
-			res.send({ error: err.message });
-		}
-		let values: any[] = [];
-		const dataFromMongo = post.toJSON();
-		const dataToCsv = dataFromMongo.flats.map((item: flat) => {
-			const id = getNameOfFlat(item.id);
-			values.push({
-				opis: "nazwa",
-				value: id,
-			});
-			values.push({
-				opis: "Licznik elektryczny 1",
-				value: item.electricityOne,
-			});
-			values.push({
-				opis: "Licznik elektryczny 2",
-				value: item.electricityTwo,
-			});
-			values.push({
-				opis: "Licznik zimnej wody",
-				value: item.coldWater,
-			});
-			values.push({
-				opis: "Licznik ciepłej wody",
-				value: item.hotWater,
-			});
-			values.push({
-				opis: "Licznik gazu lub ciepłomierz",
-				value: item.heat,
-			});
-		});
-		const csv = parse(values);
-		res.attachment("plik.csv");
-		res.status(200).send(csv);
-	});
-}
-
 function getNameOfFlat(id: number): string {
 	let newId = id;
 	id >= 1 && id < 7 ? (newId = 2) : (newId = id);
@@ -73,10 +34,62 @@ function getNameOfFlat(id: number): string {
 	}
 	return "Error";
 }
+export default function getCSV(req: Request, res: Response) {
+	data.findOne({}, {}, { sort: { createdAt: -1 } }, function (
+		err: Error,
+		post
+	) {
+		if (err) {
+			res.send({ error: err.message });
+		}
+		const values: any[] = [];
+		const dataFromMongo = post.toJSON();
+		const dataToCsv = dataFromMongo.flats.map((item: Flat | House) => {
+			const id = getNameOfFlat(item.id);
+			values.push({
+				opis: "nazwa",
+				value: id,
+			});
+			values.push({
+				opis: "Licznik elektryczny 1",
+				value: item.electricityOne,
+			});
+			values.push({
+				opis: "Licznik zimnej wody",
+				value: item.coldWater,
+			});
+			values.push({
+				opis: "Licznik ciepłej wody",
+				value: item.hotWater,
+			});
+			values.push({
+				opis: "Licznik gazu lub ciepłomierz",
+				value: item.heat,
+			});
+			if ((item as House).electricityTwo) {
+				values.push({
+					opis: "Licznik elektryczny 2",
+					value: (item as House).electricityTwo,
+				});
+				values.push({
+					opis: "Licznik elektryczny - Części  wspólne 1",
+					value: (item as House).togetherOne,
+				});
+				values.push({
+					opis: "Licznik elektryczny - Części  wspólne 2",
+					value: (item as House).togetherTwo,
+				});
+			}
+		});
+		const csv = parse(values);
+		res.attachment("plik.csv");
+		res.status(200).send(csv);
+	});
+}
 
 function flat(array: any = []): any[] {
-	let newArray = [];
-	for (let item of array) {
+	const newArray = [];
+	for (const item of array) {
 		if (Array.isArray(item)) {
 			item.map((x) => {
 				newArray.push(x);
